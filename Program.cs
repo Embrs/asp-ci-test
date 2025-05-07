3d73b9b615e9ea907a2cee78f4acb0aa8f1a8d7b
Harry <ddembrs@gmail.com>
2025-05-07 11:51:18 +0800

    backup

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using TodoApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 設置配置
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 
 // 註冊 CORS 服務
@@ -16,15 +17,18 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddAppServices(builder.Configuration); // 註冊應用服務
+// 註冊應用服務與 JWT 認證等
+builder.Services.AddAppServices(builder.Configuration);
 
 var app = builder.Build();
+
 // 啟用 CORS 中介軟體
 app.UseCors();
 
+// 使用中介軟體
 app.UseAppMiddlewares();
 
-// ✅ 註冊 API endpoints
+// 註冊 API 端點
 app.MapAuthEndpoints();
 app.MapTodoEndpoints();
 app.MapUserEndpoints();
