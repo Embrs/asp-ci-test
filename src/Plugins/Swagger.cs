a0d671f33f6fed43a45a19dfeaa8c79451b2d984
Harry <ddembrs@gmail.com>
2025-05-14 16:09:27 +0800

    backup

--- src/Plugins/Swagger.cs
+++ src/Plugins/Swagger.cs
@@ -7,26 +7,26 @@ public static class SwaggerPlugins {
   // Swegger
   public static void SettingSwagger (this IServiceCollection services, IConfiguration configur) {
     services.AddEndpointsApiExplorer();
-
+    
     services.AddSwaggerGen(options => {
-      options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
-        Description = "JWT 授權 (輸入格式: Bearer {token})",
+      options.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1" });
+
+      var jwtScheme = new OpenApiSecurityScheme {
         Name = "Authorization",
         In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-      });
-
-      options.AddSecurityRequirement(new OpenApiSecurityRequirement {{
-        new OpenApiSecurityScheme {
-          Reference = new OpenApiReference {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-          }
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "輸入以 `Bearer <token>` 的格式",
+        Reference = new OpenApiReference {
+          Type = ReferenceType.SecurityScheme,
+          Id = JwtBearerDefaults.AuthenticationScheme,
         },
-        Array.Empty<string>()
-      }});
-  });
+      };
+
+      options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, jwtScheme);
+      options.AddSecurityRequirement(new OpenApiSecurityRequirement {{ jwtScheme, Array.Empty<string>() }});
+    });
   }
 
   public static void InitSwagger(this WebApplication app) {
